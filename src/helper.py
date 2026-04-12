322e5985163e0cf3c91bd5bed347823588707640
ashvini kumar gautam <ashvini1275@gmail.com>
2026-04-12 21:29:53 +0530

    one last try

--- src/helper.py
+++ src/helper.py
@@ -1,20 +1,29 @@
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
 
-# extract text from pdf files
+
 def load_pdf_file(data):
-    loader = DirectoryLoader(data, glob="**/*.pdf", loader_cls=PyPDFLoader)
+    loader = DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
     documents = loader.load()
     return documents
 
-# split the data into text chunks
-def text_split(extracted_data):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
-    texts_chunks = text_splitter.split_documents(extracted_data)
-    return texts_chunks
 
-# download embeddings from huggingface 
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20
+    )
+    text_chunks = text_splitter.split_documents(extracted_data)
+    return text_chunks
+
+
 def download_hugging_face_embaddings():
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
     return embeddings
