97ec338aec87edcfe1c005ee35a99c2215f8fe89
ashvini kumar gautam <ashvini1275@gmail.com>
2026-09-08 01:08:39 +0530

    change groq model

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 # Make model configurable from Render
 GROQ_MODEL = os.getenv(
     "GROQ_MODEL",
-    "llama-3.3-70b-versatile"
+    "openai/gpt-oss-120b"
 )
 
 
